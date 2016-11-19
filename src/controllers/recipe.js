c2f91268d0247d01017445e9e139a812915065a0
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-11-19 23:11:51 +0100

    Added endpoints

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -1,6 +1,43 @@
 module.exports.set = function(app, driver) {
 
-  app.get('/recipe', (req, res) => {
-    res.send('Yo hoe!');
+  // List all recipes
+  app.get('/recipes', (req, res) => {
+    var session = driver.session();
+
+    session
+      .run( `MATCH (n:Recipe) RETURN n`)
+      .then( function( result ) {
+        session.close();
+        res.send(result);
+      })
+      .catch(function(error) {
+        session.close();
+        console.log(error);
+      });
+  });
+
+  // Create a Recipe authored by a User
+  app.post('/recipe', (req, res) => {
+    var session = driver.session();
+
+    var userName = req.body.username,
+        title = req.body.title,
+        ingredients = req.body.ingredients,
+        steps = req.body.steps;
+
+    session
+      .run( `
+        MATCH (n:User) WHERE n.username="${userName}"
+        CREATE (a:Recipe { title:'${title}', ingredients:'${ingredients}', steps:'${steps}'})
+        CREATE (n)-[:AUTHOR]->(a)
+        `)
+      .then( function( result ) {
+        session.close();
+        res.send(result);
+      })
+      .catch(function(error) {
+        session.close();
+        console.log(error);
+      });
   });
 }
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,40 @@
 module.exports.set = function(app, driver) {
 
+  // List all Users
+  app.get('/users', (req, res) => {
+    var session = driver.session();
+
+    session
+      .run( `MATCH (n:User) RETURN n`)
+      .then( function( result ) {
+        session.close();
+        res.send(result);
+      })
+      .catch(function(error) {
+        session.close();
+        console.log(error);
+      });
+  });
+
+  // Create a User
+  app.post('/user/:username', (req, res) => {
+    var session = driver.session();
+
+    var username = req.params.username;
+
+    session
+      .run( `CREATE (a:User { username:'${username}'})`)
+      .then( function( result ) {
+        session.close();
+        res.send(result);
+      })
+      .catch(function(error) {
+        session.close();
+        console.log(error);
+      });
+  });
+
+  // Get info of one User
   app.get('/user/:name', (req, res) => {
     var session = driver.session();
 
