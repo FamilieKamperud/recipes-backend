473ce3be0f1e3a92c3902ef6fb005856adbe32bb
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-11-20 21:27:00 +0100

    Added uuid for recipes

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -1,3 +1,5 @@
+var uuid = require('uuid');
+
 module.exports.set = function(app, driver) {
 
   // List all recipes
@@ -23,21 +25,38 @@ module.exports.set = function(app, driver) {
     var userName = req.body.username,
         title = req.body.title,
         ingredients = req.body.ingredients,
-        steps = req.body.steps;
+        steps = req.body.steps,
+        id = uuid.v4();
 
     session
       .run( `
         MATCH (n:User) WHERE n.username="${userName}"
-        CREATE (a:Recipe { title:'${title}', ingredients:'${ingredients}', steps:'${steps}'})
+        CREATE (a:Recipe { title:'${title}', ingredients:'${ingredients}', steps:'${steps}', uniqueId:'${id}'})
         CREATE (n)-[:AUTHOR]->(a)
         `)
-      .then( function( result ) {
+      .then( ( result ) => {
         session.close();
         res.send(result);
       })
-      .catch(function(error) {
+      .catch( (error) => {
         session.close();
         console.log(error);
       });
   });
+
+  // Get one recipe
+  // app.get('/recipe/:identifier', (req, res) => {
+  //   var session = driver.session();
+  //
+  //   session
+  //     .run( `MATCH (n:Recipe) WHERE n.identifier = "something" RETURN n`)
+  //     .then( function( result ) {
+  //       session.close();
+  //       res.send(result);
+  //     })
+  //     .catch(function(error) {
+  //       session.close();
+  //       console.log(error);
+  //     });
+  // });
 }
