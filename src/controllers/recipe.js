d9311c73468cd8f2f75cf0047d4253c65cbf69e9
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-11-30 22:34:35 +0100

    Added endpoint for updating a recipe

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -7,7 +7,7 @@ module.exports.use = function(app, driver) {
     var session = driver.session();
 
     session
-      .run( `MATCH (n:Recipe) RETURN n`)
+      .run( `MATCH (n:Recipe {active:'true'}) RETURN n`)
       .then( function( result ) {
         session.close();
         res.send(result);
@@ -33,7 +33,7 @@ module.exports.use = function(app, driver) {
     session
       .run( `
         MATCH (n:User) WHERE n.username="${userName}"
-        CREATE (a:Recipe { title:'${title}', ingredients:'${ingredients}', steps:'${steps}', uniqueid:'${id}'})
+        CREATE (a:Recipe { title:'${title}', ingredients:'${ingredients}', steps:'${steps}', uniqueid:'${id}', active:'true', author:'${userName}'})
         CREATE (n)-[:AUTHOR]->(a)
         `)
       .then( ( result ) => {
@@ -53,12 +53,52 @@ module.exports.use = function(app, driver) {
     var id = req.params.id;
 
     session
-      .run( `MATCH (n:Recipe) WHERE n.uniqueid = '${id}' RETURN n`)
+      .run( `MATCH (n:Recipe {uniqueid:'${id}', active:'true'}) RETURN n`)
       .then( function( result ) {
         session.close();
         res.send(result);
       })
       .catch(function(error) {
+        session.close();
+        console.log(error);
+      });
+  });
+
+  // Edit a Recipe authored by a User
+  app.put('/recipe/:id', (req, res) => {
+    var session = driver.session();
+
+    var userName = req.body.username,
+        title = req.body.title,
+        ingredients = req.body.ingredients,
+        steps = req.body.steps,
+        id = req.params.id;
+
+
+    session
+      .run( `
+        MATCH (user:User) WHERE user.username="${userName}"
+        MATCH (old:Recipe {uniqueid:'${id}', active:'true'})
+        MATCH (p:User)-[r:AUTHOR]->(old:Recipe)
+        SET old.active='false'
+        CREATE (new:Recipe {
+          title:'${title}',
+          ingredients:'${ingredients}',
+          steps:'${steps}',
+          uniqueid:'${id}',
+          active:'true',
+          author:'${userName}'
+        })
+        CREATE (user)-[:AUTHOR]->(new)
+        CREATE (new)-[:PREVIOUS_VERSION]->(old)
+        DELETE r
+        RETURN new
+        `)
+      .then( ( result ) => {
+        session.close();
+        res.send(result);
+      })
+      .catch( (error) => {
         session.close();
         console.log(error);
       });
